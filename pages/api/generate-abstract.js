86a99a91ef81aed716bc68bf3acfca6c4e9fb085
BRxAD <51241393+BRxAD@users.noreply.github.com>
2025-04-27 10:37:51 -0400

    Update generate-abstract.js

--- pages/api/generate-abstract.js
+++ pages/api/generate-abstract.js
@@ -45,7 +45,6 @@ export default async function handler(req, res) {
     const dalleResponse = await openai.images.generate({
       model: "dall-e-3",
       prompt: finalPrompt,
-      n: 1,
       size: "1024x1024",
     });
 
